410ea631d1cb84db10e061cd1a56cc209713c5a1
Eddie Moon <eddiemoonie@gmail.com>
2018-05-16 18:50:37 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,5 +3,5 @@ CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT,
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
-CREATE TABLE pledges (id INTEGER PRIMARY KEY, user_id INTEGER,
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount, INTEGER, user_id INTEGER,
   project_id INTEGER);
